2784001b84a6e98147f130b601b4c1df75c0b1ed
andydevop <56658287+andydevop@users.noreply.github.com>
2019-11-02 21:09:01 -0500

    0908PMCST-Nov02-2019-Commit#5- Built using Gitbash
    0908PMCST-Nov02-2019-Commit#5- Built using Gitbash

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is a maven project in conjunction with Github!" );
+        System.out.println( "Testing the Maven project using the Git Bash instead of CMD!" );
     }
 }

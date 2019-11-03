e88281de67728401a96159af9a99c1b477c8404a
andydevop <56658287+andydevop@users.noreply.github.com>
2019-11-02 20:40:42 -0500

    0839PM-Nov02-2019-Commit#2-Commit through Github!
    0839PM-Nov02-2019-Commit#2-Commit through Github!

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is a maven project by Adarsh! This is version 2.0" );
+        System.out.println( "Hello World! This is a maven project in conjunction with Github!" );
     }
 }
